812ac444c4fd0d1d6ae12638ad0b8d1717c0fc1c
Hala Elhamahmy <hala.elhamahmy@gmail.com>
2018-03-12 13:01:56 +0000

    Update 20180311160236_create_group_members.rb

--- db/migrate/20180311160236_create_group_members.rb
+++ db/migrate/20180311160236_create_group_members.rb
@@ -1,7 +1,7 @@
 class CreateGroupMembers < ActiveRecord::Migration[5.1]
   def change
     create_table :group_members do |t|
-      t.references :friendship, foreign_key: true
+      t.references :user, foreign_key: true
       t.references :group, foreign_key: true
 
       t.timestamps
